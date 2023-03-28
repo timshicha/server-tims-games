f25df451fa3b85829c0ee660d5cf6b6d6feb7fb5
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-03-27 19:54:10 -0700

    added express rate limit to limit number of requests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const rateLimiter = require("express-rate-limit");
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
@@ -8,14 +9,21 @@ const testRoutes = require("./routes/test.js");
 const socketIOHandler = require("./routes/ws.js").socketIOHandler;
 const prefix = "/api";
 
+const expressRateLimitor = rateLimiter({
+    max: 10,
+    windowMS: 10000,
+});
+
+
 // Set up CORS to allow requests from our client
 const cors = require("cors");
 corsOptions = {
     origin: process.env.CLIENT_URL,
     optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+};
 
+app.use(cors(corsOptions));
+app.use(expressRateLimitor);
 app.use(prefix, testRoutes);
 
 socketIOHandler(http, corsOptions);
